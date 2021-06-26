62d880abb56d88501edc9a82f8c3db549048221f
lzhang693 <lzhang693@gatech.edu>
2021-06-25 18:44:57 -0700

    github repo component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
 import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 import { getProfileById } from '../../actions/profile';
 
 
@@ -52,6 +53,10 @@ const Profile = ({
                             </Fragment>
                         ) : (<h4>No education added yet</h4>)}
                     </div>
+
+                    {profile.githubusername && (
+                        <ProfileGithub username={profile.githubusername} />
+                    )}
                 </div>
             </Fragment> }
         </Fragment>
