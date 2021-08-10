47230d5e63743ed4790e7e62726c404da372e44e
lzhang693 <lzhang693@gatech.edu>
2021-08-09 21:03:36 -0700

    fix delete profile education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -311,13 +311,18 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
         const profile = await Profile.findOne({ user: req.user.id });
 
         // Get remove index
-        const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id);
+        const eduIds = profile.education.map(edu => edu._id.toString());
+        const removeIndex = eduIds.indexOf(req.params.edu_id);
 
         profile.education.splice(removeIndex, 1);
 
-        await profile.save();
-
-        res.json(profile);
+        if (removeIndex === -1) {
+            return res.status(500).send('Server Error');
+        } else {
+            profile.education.splice(removeIndex, 1);
+            await profile.save();
+            return res.status(200).json(profile);
+        }
     } catch (err) {
         console.err(err.message);
         res.status(500).send('Server Error');
