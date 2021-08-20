4398141721d81b4d3b29a7711e2a5d7dee9e8bb4
lzhang693 <lzhang693@gatech.edu>
2021-08-20 11:29:44 -0700

    minor fix in profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -13,7 +13,7 @@ import { getProfileById } from '../../actions/profile';
 
 const Profile = ({ 
     getProfileById, 
-    profile: { profile, loading }, 
+    profile: { profile }, 
     auth, 
     match 
 }) => {
@@ -23,44 +23,59 @@ const Profile = ({
 
     return (
         <Fragment>
-            { profile === null || loading ? <Spinner /> : <Fragment>
-                <Link to='/profiles' className='btn btn-light'> Back To Profiles </Link>
-                { auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (
-                    <Link to='/edit-profile' className='btn btn-dark'>
-                        Edit profile
-                    </Link>
-                )}
-                <div className="profile-grid my-1">
-                    <ProfileTop profile={ profile } />
-                    <ProfileAbout profile={ profile } />
-                    <div className='profile-exp bg-white pp-2'>
-                        <h2 className='text-primary'>Experience</h2>
-                        {profile.experience.length > 0 ? (
-                            <Fragment>
-                                { profile.experience.map(experience => (
-                                    <ProfileExperience key={experience._id} experience={experience} />
-                                ))}
-                            </Fragment>
-                        ) : (<h4>No experience added yet</h4>)}
-                    </div>
-                    <div className='profile-edu bg-white pp-2'>
-                        <h2 className='text-primary'>Education</h2>
-                        {profile.education.length > 0 ? (
-                            <Fragment>
-                                { profile.education.map(education => (
-                                    <ProfileEducation key={education._id} education={education} />
-                                ))}
-                            </Fragment>
-                        ) : (<h4>No education added yet</h4>)}
-                    </div>
-
-                    {profile.githubusername && (
-                        <ProfileGithub username={profile.githubusername} />
+            { profile === null ? (
+                <Spinner />
+            ) : (
+                <Fragment>
+                    <Link to='/profiles' className='btn btn-light'> Back To Profiles </Link>
+                    { auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (
+                        <Link to='/edit-profile' className='btn btn-dark'>
+                            Edit profile
+                        </Link>
                     )}
-                </div>
-            </Fragment> }
+                    <div className="profile-grid my-1">
+                        <ProfileTop profile={ profile } />
+                        <ProfileAbout profile={ profile } />
+                        <div className='profile-exp bg-white pp-2'>
+                            <h2 className='text-primary'>Experience</h2>
+                            {profile.experience.length > 0 ? (
+                                <Fragment>
+                                    { profile.experience.map(experience => (
+                                        <ProfileExperience 
+                                            key={experience._id} 
+                                            experience={experience}
+                                        />
+                                    ))}
+                                </Fragment>
+                            ) : (
+                                <h4>No experience added</h4>
+                            )}
+                        </div>
+
+                        <div className='profile-edu bg-white pp-2'>
+                            <h2 className='text-primary'>Education</h2>
+                            {profile.education.length > 0 ? (
+                                <Fragment>
+                                    { profile.education.map(education => (
+                                        <ProfileEducation 
+                                            key={education._id} 
+                                            education={education} 
+                                        />
+                                    ))}
+                                </Fragment>
+                            ) : (
+                                <h4>No education added</h4>
+                            )}
+                        </div>
+
+                        {profile.githubusername && (
+                            <ProfileGithub username={profile.githubusername} />
+                        )}
+                    </div>
+                </Fragment> 
+            )}
         </Fragment>
-    )
+    );
 };
 
 Profile.propTypes = {
